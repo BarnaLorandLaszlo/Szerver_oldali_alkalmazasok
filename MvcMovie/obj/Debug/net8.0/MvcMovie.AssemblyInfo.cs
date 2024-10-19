e54396300f3dc4e5ea0381cb69fdb636406389ac
Barna Loránd László <traktor.mtz1970@gmail.com>
2024-10-19 19:11:05 +0200

    Controller actions and views

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd67fe10b54d9278d6d86f7023f6fea8cd3fc79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ded8b5b681ca75974b6a5e37058d604406308e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
